6d0f8d4173f34ab2bdf5c4fbf9259b5ab010880f
Ouafae Haddouchi <t-ouhadd@microsoft.com>
2019-06-04 10:05:52 -0400

    revert to older commit.

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,11 @@
 // Import the module and reference it with the alias vscode in your code below
 // import * as vscode from 'vscode';
 import 'vscode';
-import { workspace, window, languages, commands, StatusBarAlignment, DocumentSelector, ExtensionContext, InputBoxOptions} from 'vscode';
+import { workspace, window, languages, commands, StatusBarAlignment, DocumentSelector, ExtensionContext} from 'vscode';
 import { SystemVerilogDefinitionProvider } from './providers/DefintionProvider';
 import { SystemVerilogDocumentSymbolProvider } from './providers/DocumentSymbolProvider';
 import { SystemVerilogHoverProvider } from './providers/HoverProvider';
 import { SystemVerilogWorkspaceSymbolProvider } from './providers/WorkspaceSymbolProvider';
-import { SystemVerilogModuleInstantiator } from './providers/ModuleInstantiator';
 
 
 // this method is called when your extension is activated
@@ -32,7 +31,6 @@ export function activate(context: ExtensionContext) {
         settings.get('systemverilog.parallelProcessing'));
     let defProvider = new SystemVerilogDefinitionProvider(symProvider, docProvider);
     let hoverProvider = new SystemVerilogHoverProvider(symProvider, docProvider);
-    let moduleInstantiator = new SystemVerilogModuleInstantiator(symProvider, docProvider);
 
     context.subscriptions.push(statusBar);
     context.subscriptions.push(languages.registerDocumentSymbolProvider(selector, docProvider));
@@ -40,7 +38,6 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(languages.registerHoverProvider(selector, hoverProvider));
     context.subscriptions.push(languages.registerWorkspaceSymbolProvider(symProvider));
     context.subscriptions.push(commands.registerCommand('systemverilog.build_index', rebuild));
-    context.subscriptions.push(commands.registerCommand('systemverilog.auto_instantiate', instantiate));
     // WIP
     // window.registerTreeDataProvider('systemverilogModules', new SystemVerilogTreeDataProvider())
     // window.registerTreeDataProvider('systemverilogDocuementSymbols', new SystemVerilogDocumentSymbolTreeProvider())
@@ -54,35 +51,6 @@ export function activate(context: ExtensionContext) {
             symProvider.parallelProcessing = settings.get('systemverilog.parallelProcessing');
             symProvider.build_index()
         }
-    }
-
-    function instantiate(){
-        let options: InputBoxOptions = {
-            prompt: "Please enter the module's name: ",
-            placeHolder: "variable here ..."
-        }
-        
-        //request the module's name from the user
-        window.showInputBox(options).then(value => {
-            if (!value){
-               return; 
-            } 
-            // current editor
-            const editor = window.activeTextEditor;
-
-            // check if there is no selection
-            if (editor.selection.isEmpty) {
-            // the Position object gives you the line and character where the cursor is
-            const position = editor.selection.active;
-
-                if (editor) {
-                    // add text to the current selection
-                    editor.edit(editBuilder => {
-                        editBuilder.replace(editor.selection, moduleInstantiator.auto_instantiate(value));
-                    });
-                }
-            }
-        });
     }
 }
 
